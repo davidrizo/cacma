422528411f76cf4cd273cde44179d90e73c5c84e
davidrizo <drizovalero@gmail.com>
2020-05-05 21:29:52 +0200

    Production release

--- develop/frontend/src/environments/environment.prod.ts
+++ develop/frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // important not to finish the URL with slash
-  apiEndpoint: 'http://easda.es/investigacion/cacma/api',
+  apiEndpoint: 'https://cacma.000webhostapp.com/api',
   client_id: 'rgueuEzyc9LAVIhNjCLTKeYG4O53dTvE', // auth0 client ID
   home: 'https://davidrizo.github.io/cacma'
 };
