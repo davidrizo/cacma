7d53c46c94ce43cce0e7888950c8f64c00a35514
davidrizo <drizovalero@gmail.com>
2020-05-20 08:12:09 +0200

    ...

--- develop/php_backend/api/apirest.php
+++ develop/php_backend/api/apirest.php
@@ -2,4 +2,6 @@
 
 ini_set('display_errors',0);
 
-
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
