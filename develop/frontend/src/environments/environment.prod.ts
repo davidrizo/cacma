c47e2cd09a7dcf94618d72f072b55fd0b57d23ae
davidrizo <drizovalero@gmail.com>
2021-03-01 19:01:04 +0100

    Production version

--- develop/frontend/src/environments/environment.prod.ts
+++ develop/frontend/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
   // important not to finish the URL with slash
   apiEndpoint: 'https://research.easda.es/cacma/grecoh/api',
-  home: 'https://davidrizo.github.io/cacma',
+  home: 'https://research.easda.es/cacma',
   auth: {
     CLIENT_ID: 'rgueuEzyc9LAVIhNjCLTKeYG4O53dTvE',
     CLIENT_DOMAIN: 'easda-research.eu.auth0.com', // e.g., 'you.auth0.com'
-    REDIRECT: 'https://davidrizo.github.io/cacma/callback',
-    LOGOUT_URL: 'https://davidrizo.github.io/cacma'
+    REDIRECT: 'https://research.easda.es/cacma/callback',
+    LOGOUT_URL: 'https://research.easda.es/cacma'
   }
 };
